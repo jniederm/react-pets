9cd866622ef922ba59d9e26417eddc038376231a
Jakub Niedermertl <jniederm@redhat.com>
2018-01-25 15:18:49 +0100

    > Solution Redux 2

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,15 @@
-import { ADD_PET } from '../constants'
+import { ADD_PET, REMOVE_PET } from '../constants'
 
 export function addPet(pet) {
   return {
     type: ADD_PET,
     payload: pet
+  }
+}
+
+export function removePet(petId) {
+  return {
+    type: REMOVE_PET,
+    payload: petId
   }
 }
