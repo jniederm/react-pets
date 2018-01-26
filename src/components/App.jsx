aa5e226dbd0950377971fb5e8e6df4e97cac7b1a
Bohdan Iakymets <biakymet@redhat.com>
2018-01-26 02:53:28 +0100

    > Assingment React 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
-import { connect } from 'react-redux'
-
-import Header from './Header'
-import Grid from './Grid'
 
 class App extends React.Component {
 
   render() {
     return (
       <React.Fragment>
-
-        <Header />
-        <Grid pets={this.props.pets} />
-
       </React.Fragment>
     )
   }
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
-import { randomPet } from './components/utils'
-
-const pets = {
-  1: randomPet(),
-  2: randomPet(),
-  3: randomPet()
-}
 
 ReactDOM.render(
-  <App pets={pets} />,
+  <App />,
   document.getElementById('app')
 )
