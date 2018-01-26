4a16bbaad89c3b6ddca57154472b96c8b6951d7e
Bohdan Iakymets <biakymet@redhat.com>
2018-01-26 02:55:16 +0100

    > Solution React 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from './Header'
 
 class App extends React.Component {
 
   render() {
     return (
       <React.Fragment>
+        <Header />
       </React.Fragment>
     )
   }
