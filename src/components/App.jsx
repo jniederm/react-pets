bd56688be40d2bf2df0ee5f76c6434db360c25b5
Bohdan Iakymets <biakymet@redhat.com>
2018-01-26 02:57:01 +0100

    > Assingment React 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,2 @@
 import React from 'react'
-import Header from './Header'
 
-class App extends React.Component {
-
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-      </React.Fragment>
-    )
-  }
-
-}
-
-App.propTypes = {}
-
-export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,2 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-)
